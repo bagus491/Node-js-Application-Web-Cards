627ebf5dc1228d658d044562470948304da99538
bagus491 <baguskalam491@gmail.com>
2023-07-12 22:02:08 +0700

    selesai verifikasi token dan id disemua app.get

--- src/Controllers/UserControllers.js
+++ src/Controllers/UserControllers.js
@@ -11,34 +11,90 @@ const DataUsers = (req,res) => {
 
 //home
 const HomeWeb = (req,res) => {
-    res.render('home', {
-        title: 'halaman/home',
-        layout: 'main-layouts/main-layouts'
-    })
+    const token = req.cookies.token
+    if(token){
+        const dataOk = req.cookies.id
+        if(dataOk){
+            res.redirect('/profile')
+        }else{
+            res.render('home', {
+                title: 'halaman/home',
+                layout: 'main-layouts/main-layouts'
+            })
+        }
+    }else{
+        res.render('home', {
+            title: 'halaman/home',
+            layout: 'main-layouts/main-layouts'
+        })
+    }
 }
 
 //login
 const LoginUsers = (req,res) => {
-    res.render('login', {
-        title: 'halaman/login',
-        layout: 'login',
-    })
+    const token = req.cookies.token
+    if(token){
+        const dataOk = req.cookies.id
+        if(dataOk){
+            res.redirect('/profile')
+        }else{
+            res.render('login', {
+                title: 'halaman/login',
+                layout: 'login',
+            })
+        }
+    }else{
+        res.render('login', {
+            title: 'halaman/login',
+            layout: 'login',
+        })
+    }
+   
 }
 
 //register
 const RegisterUsers = (req,res) => {
+    const token = req.cookies.token
+    if(token){
+        const dataOk = req.cookies.id
+        if(dataOk){
+            res.redirect('/profile')
+        }else{
+            res.render('register', {
+                title: 'halaman/register',
+                layout: 'register',
+            })
+        }
+    }else{
         res.render('register', {
             title: 'halaman/register',
             layout: 'register',
         })
+    }
+        
 }
 
 //profile
 const RegisterProfile = (req,res) => {
-        res.render('profile', {
-            title: 'halaman/profile',
-            layout: 'main-layouts/main-layouts',
-         })
+    const token = req.cookies.token
+    if(token){
+        const dataOk = req.cookies.id
+        if(dataOk){
+            res.render('profile', {
+                title: 'halaman/profile',
+                layout: 'main-layouts/main-layouts',
+             })
+        }else{
+            res.clearCookie('token')
+            res.clearCookie('id')
+            res.redirect('/login')
+        }
+    }else{
+        res.clearCookie('token')
+        res.clearCookie('id')
+        res.redirect('/login')
+    }
+       
 }
 
 
