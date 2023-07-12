de6fc64095c300f04ba75b1c58720bd467386bb9
bagus491 <baguskalam491@gmail.com>
2023-07-12 16:29:52 +0700

    menambahkan middleware di App

--- App.js
+++ App.js
@@ -2,8 +2,18 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const path = require('path')
 
+//midleeware
+app.use(express.urlencoded({extended:true}))
+app.set(path.join(__dirname, 'src/views'))
+app.set(path.join(__dirname, 'src/public'))
+app.set(path.join(__dirname, 'src/router'))
 
+//middle
+const mainlayouts = require('express-ejs-layouts')
+app.set('view engine', 'ejs')
+app.use(mainlayouts)
 
 
 
